c50963ad1e47f7090d169107b60eb497e691c1c1
Konrad Czajka <konrad.czajka@schange.com>
2020-06-05 20:56:44 +0200

    Add empty Springboot project

---
+++ src/main/java/konrad/czajka/springbootclouddemo/SpringbootCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package konrad.czajka.springbootclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/konrad/czajka/springbootclouddemo/SpringbootCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package konrad.czajka.springbootclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
